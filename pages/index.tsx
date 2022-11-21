50229eb2c8160ffe6173b78e99c4df7ae5b2f45a
Javar Alexander <javar.alexander@gmail.com>
2022-11-20 22:38:02 -0500

    chore: clean up pt 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,125 +1,14 @@
 import Layout from "../components/Layouts";
-
 import { useCallback, useState } from "react";
 import { flushSync } from "react-dom";
 import Modal from "../components/Winner";
 import {
   GridState,
   GridState as GridStateObject,
-  State,
-  Winner,
-} from "../util/types";
+} from "../util/types/types";
 
-const lastRows = [35, 36, 37, 38, 39, 40, 41];
-const indexColumnMap = {
-  0: [0, 7, 14, 21, 28, 35],
-  1: [1, 8, 15, 22, 29, 36],
-  2: [2, 9, 16, 23, 30, 37],
-  3: [3, 10, 17, 24, 31, 38],
-  4: [4, 11, 18, 25, 32, 39],
-  5: [5, 12, 19, 26, 33, 40],
-  6: [6, 13, 20, 27, 34, 41],
-};
+import { lastRows, indexColumnMap, PLAYER_1, PLAYER_2, checkWinConditions } from "../util";
 
-const PLAYER_1 = "Player_1";
-const PLAYER_2 = "Player_2";
-
-const checkColumnRecursively: Boolean = (
-  element: Number,
-  array: [],
-  moveBy: Number,
-  counter: Number = 1
-) => {
-  const nextElement = element + moveBy;
-  // counter is 4, we've had four consecutive hits.
-  if (counter == 4) {
-    return true;
-  }
-
-  if (!array.length > 0) {
-    return false;
-  }
-
-  // check the array for next next element. if it exists, increment the counter, remove the next element
-  // from the array and call the function again.
-  if (array.includes(nextElement)) {
-    counter = counter + 1;
-    array.shift();
-    return checkColumnRecursively(nextElement, array, moveBy, counter);
-  } else {
-    return false;
-  }
-};
-
-const checkFourInARow = (
-  state: State,
-  currentPlayer: String,
-  moveBy: Number
-) => {
-  let won = false;
-  const currentUserGameState: State = state.filter(
-    (s: GridStateObject) => s.user === currentPlayer
-  );
-
-  if (currentUserGameState.length < 4) {
-    return false;
-  }
-
-  const currentUserBoardPositions: Array<Number> = currentUserGameState
-    .map(({ cell }) => cell)
-    .sort((a, b) => a - b);
-
-  if (currentUserBoardPositions.length < 4) {
-    return false;
-  }
-
-  // elements are sorted
-  // check one element, see if there is one x spaces away
-  // if x spaces away, then grab that element and check to see if there is another one x spaces away(recursion)
-  // if not x spaces, keep going
-
-  currentUserBoardPositions.forEach((element) => {
-    const winConditionMet = checkColumnRecursively(
-      element,
-      [...currentUserBoardPositions],
-      moveBy
-    );
-    if (winConditionMet) {
-      won = true;
-      return;
-    }
-  });
-
-  return won;
-};
-
-const checkWinConditions = (
-  state: State,
-  currentPlayer: String,
-  callback: React.Dispatch<React.SetStateAction<Winner>>
-) => {
-  // vertical check
-  if (checkFourInARow(state, currentPlayer, 7)) {
-    callback({ player: currentPlayer, won: true });
-    return true;
-  }
-
-  // horizontal check
-  if (checkFourInARow(state, currentPlayer, 1)) {
-    callback({ player: currentPlayer, won: true });
-    return true;
-  }
-
-  // diag left & right
-  if (
-    checkFourInARow(state, currentPlayer, 6) ||
-    checkFourInARow(state, currentPlayer, 8)
-  ) {
-    callback({ player: currentPlayer, won: true });
-    return true;
-  }
-  return false;
-};
 
 export default function Home() {
   const [gridState, setGridState] = useState([]);
